8796c7b8f5770230d8f2887fd88f662a1c5a82d5
dino <dino@330z.net>
2018-01-03 00:58:37 +1100

    Implemented inline mode
    Cleanup code
    Messed around with the convos

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const Markup = require('telegraf/markup')
 const BOT_TOKEN = process.env.BOT_TOKEN
 const PROVIDER_TOKEN = process.env.PROVIDER_TOKEN
 const MYID = process.env.MYID
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 443
 
-const bot = new Telegraf(BOT_TOKEN, {username: 'TheWiseBot'})
+const bot = new Telegraf(BOT_TOKEN)
+
+//Set the bot name
+bot.telegram.getMe().then((botInfo) => {
+    console.log(botInfo.username + ' is running on port ' + PORT)
+    bot.options.username = botInfo.username
+  })
+  
 //So I can specfy a port
 bot.startWebhook('/bot${BOT_TOKEN}', null, PORT)
 
@@ -17,7 +24,9 @@ bot.start((ctx) => {
     msg += "\n\n"
     msg += 'You can also \/draw a 🃏 from a stack. If you wish you can enable \/singlestack mode to \/draw 🃏 from the same stack until you \/shuffle the stack.'
     msg += "\n\n"
-    msg += '\/suggest your favourite decision making method if I can\'t provide you yet.'
+    msg += 'Or I can help you \/pick one from your choices'
+    msg += "\n\n"
+    msg += '\/suggest your favourite decision making method if I can\'t provide you yet, the universe may accept your suggestions'
     msg += "\n\n"
     msg += 'Ask \/help for more.'
     return ctx.reply(msg)
@@ -44,9 +53,12 @@ bot.command('help', (ctx) =>{
     msg += "\n"
     msg += '\/shuffle - reset the stack when \/singlestack is toggled'
     msg += "\n\n"
+    msg += '*Choices*'
+    msg += '\/pick - make the decision from your choices'
+    msg += "\n\n"
     msg += '*Others*'
     msg += "\n"
-    msg += '\/suggest - suggest a decision making method'
+    msg += '\/suggest - suggest a decision making method, or share me you view of universe'
     msg += "\n"
     msg += '\/nerddrink - buy the wise a nerd drink, he needs energy to make decisions'
     
@@ -54,25 +66,19 @@ bot.command('help', (ctx) =>{
 })
 
 //Coin
-bot.command('flip', (ctx) =>{
-    //flip a coin
-    var coin = (Math.random() >= 0.5 ? "Heads" : "Tails");
-    return ctx.reply(coin)
-})
+var coin = () => (Math.random() >= 0.5 ? "Heads" : "Tails")
+
+bot.command('flip', (ctx) => ctx.reply(coin()))
 
 //Dice
-bot.command('roll', (ctx) =>{
-    //roll a dics
-    return ctx.reply(Math.floor(Math.random() * 6) + 1)
-})
-
-bot.command('roll3', (ctx) =>{
-    //roll 3 dices
-    var a = Math.floor(Math.random() * 6) + 1
-    var b = Math.floor(Math.random() * 6) + 1
-    var c = Math.floor(Math.random() * 6) + 1
-    return ctx.reply(a + ' - ' + b + ' - ' + c)
-})
+var die = () => (Math.floor(Math.random() * 6) + 1)
+var dice = () => {
+    return (Math.floor(Math.random() * 6) + 1) + ' - ' + 
+            (Math.floor(Math.random() * 6) + 1) + ' - ' +
+            (Math.floor(Math.random() * 6) + 1)
+}
+bot.command('roll', (ctx) => ctx.reply(die()))
+bot.command('roll3', (ctx) => ctx.reply(dice()))
 
 //Stack
 var single_stack = false
@@ -81,20 +87,33 @@ const stack_o = ['♠️ A', '♠️ 2', '♠️ 3', '♠️ 4', '♠️ 5', '�
                 '♣️ A', '♣️ 2', '♣️ 3', '♣️ 4', '♣️ 5', '♣️ 6', '♣️ 7', '♣️ 8', '♣️ 9', '♣️ 10', '♣️ J', '♣️ Q', '♣️ K', 
                 '♦️ A', '♦️ 2', '♦️ 3', '♦️ 4', '♦️ 5', '♦️ 6', '♦️ 7', '♦️ 8', '♦️ 9', '♦️ 10', '♦️ J', '♦️ Q', '♦️ K']
 var stack = stack_o
+var card = () => {
+    if (single_stack){
+        var card = stack[Math.floor(Math.random() * stack.length)]
+        stack = stack.filter(e => e !== card)
+        return (card + "\n" + stack.length + ' card(s) left in the stack.')
+    } else {
+        var suits = ['♠️', '♥️', '♣️', '♦️']
+        var ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        return (suits[Math.floor(Math.random() * suits.length)] + ' ' + ranks[Math.floor(Math.random() * ranks.length)])
+    }
+}
+
+bot.command('draw', (ctx) => ctx.reply(card()))
 
 bot.command('singlestack', (ctx) =>{
-    //
+    //trigger single stack mode
     if (single_stack){
         single_stack = false
         return ctx.reply('Now you draw a card from a new stack every time.' + "\n" + 'Call again to enable.')
     } else {
         single_stack = true
-        return ctx.reply('Now you will never draw the same card until you \/shuffle the stack. ' + "\n" + 'Call again to enable.')
+        return ctx.reply('Now you will never draw the same card until you \/shuffle the stack. ' + "\n" + 'Call again to disable.')
     }
 })
 
 bot.command('shuffle', (ctx) =>{
-    //
+    //restore the stack in single stack mode
     if (single_stack){
         stack = stack_o
         return ctx.reply('A brand new stack has been shuffled.' + "\n" + stack.length + ' card(s) left in the stack.')
@@ -104,44 +123,40 @@ bot.command('shuffle', (ctx) =>{
     
 })
 
-bot.command('draw', (ctx) =>{
-    //draw a card from a stack
-    if (single_stack){
-        var card = stack[Math.floor(Math.random() * stack.length)]
-        stack = stack.filter(e => e !== card)
-        return ctx.reply(card + "\n" + stack.length + ' card(s) left in the stack.')
-    } else {
-        var suits = ['♠️', '♥️', '♣️', '♦️']
-        var ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-        return ctx.reply(suits[Math.floor(Math.random() * suits.length)] + ' ' + ranks[Math.floor(Math.random() * ranks.length)])
-    }
-    
-})
-
 //Pick one from list
-bot.command('pick', (ctx) =>{
-    var i = ctx.message.text.trim().indexOf(' ')
-    if (i > 0){
-        var options = (ctx.message.text.substr(i + 1)).split(',').map(item => item.trim()).filter(item => item !== '')
-        if (options.length > 0){
-            return ctx.reply(options[Math.floor(Math.random() * options.length)] + ' has to be the one. 😉')
-        } else {
-            return ctx.reply('Huh? My hearing isn\'t too well. Try again with following pattern: \/pick option1, option2, option3, ...')
-        }
-    } else {
-        return ctx.reply('Huh? My hearing isn\'t too well. Try again with following pattern: \/pick option1, option2, option3, ...')
-    }
+var invalid_choices = 'Huh? My hearing isn\'t too well. Try again with following pattern: \/pick 🅰️, 🅱️, ...'
+var one = (txt) => {
+    var options = txt.split(',').map(item => item.trim()).filter(item => item !== '')
+    if (options.length == 1)
+        return (options[0] + 'has to be the one. Unlike the universe, there\'re multiple of them existing parallely.')
+    else if (options.length > 0)
+        return ('Out of ' + options.join(', ') +'\n*'+options[Math.floor(Math.random() * options.length)] + '* has to be the one. 😉')
+    else 
+        return (invalid_choices)
+}
+bot.command('pick', (ctx) => {
+    var i = txt.trim().indexOf(' ')
+    if (i > 0)
+        return ctx.replyWithMarkdown(one(ctx.message.text.substr(i + 1)))
+    else
+        return ctx.replyWithMarkdown(invalid_choices)
+    
 })
 
 //New suggestion from users
 bot.command('suggest', (ctx) =>{
+    var invalid_suggestion = 'Huh? My hearing isn\'t too well. Try again with \/suggest and follow by your suggestions. The universe won\'t accept blank suggestions.'
     var i = ctx.message.text.trim().indexOf(' ')
     if (i > 0){
         var msg = ctx.message.text.substr(i + 1)
-        ctx.telegram.sendMessage(MYID, '@' + ctx.from.username + ' suggested to ' + msg)
-        return ctx.reply('Thanks, I will look into this method soon. 💪')
+        if (msg.trim().length > 0){
+            bot.telegram.sendMessage(MYID, '@' + ctx.from.username + ' suggested to ' + msg)
+            return ctx.reply ('May the universe be with you, I will look into this soon. 💪')
+        } else {
+            return ctx.reply (invalid_suggestion)
+        }
     } else {
-        return ctx.reply('Huh? My hearing isn\'t too well. Try again with \/suggest and follow by your suggestions')
+        return ctx.reply(invalid_suggestion)
     }
 })
 
@@ -172,12 +187,58 @@ bot.command('nerddrink', (ctx) =>{
 bot.on('pre_checkout_query', (ctx) => ctx.answerPreCheckoutQuery(true))
 bot.on('successful_payment', (ctx) =>{
     ctx.telegram.sendMessage(MYID, '@' + ctx.from.username + ' bought you a nerd drink! 🍻')
-    return ctx.reply('Thank you! 🍻'+"\n"+'As you may know, energy bill is going up crazy, the Wise cannot make it on his own. Your help is very appreciated!')
+    return ctx.reply('May the universe be with you! 🍻'+"\n"+'As you may know, energy bill is going up crazy, the Wise cannot make it on his own. Your help is very appreciated!')
 })
 
-//close event, hopefully work
-bot.on('exit', ({stop}) => stop())
-bot.on('SIGINT', ({stop}) => stop())
+//Inline mode
+bot.on('inline_query', async ({ inlineQuery, answerInlineQuery }) => {
+    var result = [{type: "article",
+                    id: "coin",
+                    title: "Heads or Tails 🌝",
+                    input_message_content: {
+                        message_text: coin(),
+                        parse_mode: 'Markdown'
+                    }
+                },
+                    {type: "article",
+                    id: "die",
+                    title: "🎲",
+                    input_message_content: {
+                        message_text: die(),
+                        parse_mode: 'Markdown'
+                    }
+                },
+                    {type: "article",
+                    id: "dice",
+                    title: "🎲🎲🎲",
+                    input_message_content: {
+                        message_text: dice(),
+                        parse_mode: 'Markdown'
+                    }
+                },
+                    {type: "article",
+                    id: "card",
+                    title: "🃏",
+                    input_message_content: {
+                        message_text: card(),
+                        parse_mode: 'Markdown'
+                    }
+                },
+                    {type: "article",
+                    id: "pick",
+                    title: "Pick from ...",
+                    description: "🅰️, 🅱️, ...",
+                    input_message_content: {
+                        message_text: one(inlineQuery.query),
+                        parse_mode: 'Markdown'
+                    }
+                }]
+    return answerInlineQuery(result, 
+        {
+            is_personal: true,
+            cache_time: 0
+        })
+})
 
 //start poll uptdates
 bot.startPolling()
